42367f18341267a29d917e752114ebdf1eb21b76
IGNACIO ROZAS LÓPEZ <Ignacio.Rozas@alu.uclm.es>
2025-01-21 17:55:26 +0100

    trafficmix completo

---
+++ clientblob.py
@@ -0,0 +1,178 @@
+from typing import List
+import requests
+from requests_toolbelt import MultipartEncoder
+
+def _assert_status_ok_(status_code):
+    if status_code not in [200, 201, 204, 400, 401, 404]:
+        raise RuntimeError('Unknown response from API')
+    else:
+        return True
+
+class Client:
+    def __init__(self, root_api: str) -> None:
+        self.root_api = root_api
+
+    def put_blob(self, auth_token: str, name: str, writer: List[str], file_path: str) -> None:
+        with open(file_path, 'rb') as file:
+            multipart = MultipartEncoder(
+                fields={
+                    'name': name,
+                    'writable_by': ', '.join(writer),
+                    'owner': writer[0],
+                    'file': (file_path, file, 'application/octet-stream')
+                }
+            )
+
+            response = requests.put(
+                f'{self.root_api}/blob',
+                headers={
+                    'AuthToken': auth_token,
+                    'Content-Type': multipart.content_type
+                },
+                data = multipart
+            )
+
+        _assert_status_ok_(response.status_code)
+        print(f'{response.status_code}, {response.text}')
+        return response.text.split(': ')[1].strip()
+
+    def delete_blob(self, blob_id: str, auth_token: str) -> None:
+        response = requests.delete(
+            f'{self.root_api}/blob/{blob_id}',
+            headers = {'AuthToken': auth_token}
+        )
+        if _assert_status_ok_(response.status_code):
+            print(f'Status Code Delete_Data Request: {response.status_code}')
+
+    def get_blob(self, blob_id: str, auth_token: str) -> None:
+        response = requests.get(
+            f'{self.root_api}/blob/{blob_id}/data',
+            headers = {'AuthToken': auth_token}
+        )
+        _assert_status_ok_(response.status_code)
+        file = response.content
+        print(f'{response.status_code}, File: {file}')
+
+    def post_patch_blob(self, blob_id: str, auth_token: str, file_path: str, request: str) -> None:
+        if request == 'POST':
+            with open(file_path, 'rb') as file:
+                multipart = MultipartEncoder(
+                    fields={
+                        'file': (file_path, file, 'application/octet-stream')
+                    }
+                )
+
+                response = requests.post(
+                    f'{self.root_api}/blob/{blob_id}/data',
+                    headers={
+                        'AuthToken': auth_token,
+                        'Content-Type': multipart.content_type
+                    },
+                    data = multipart
+                )
+
+            _assert_status_ok_(response.status_code)
+            print(f'{response.status_code}, {response.text}')
+
+        elif request == 'PATCH':
+            with open(file_path, 'rb') as file:
+                multipart = MultipartEncoder(
+                    fields={
+                        'file': (file_path, file, 'application/octet-stream')
+                    }
+                )
+                
+                response = requests.patch(
+                    f'{self.root_api}/blob/{blob_id}/data',
+                    headers={
+                        'AuthToken': auth_token,
+                        'Content-Type': multipart.content_type
+                    },
+                    data = multipart
+                )
+            _assert_status_ok_(response.status_code)
+            print(f'{response.status_code}, {response.text}')
+
+        else:
+            raise ValueError('Unknown request')
+
+    def get_roles_blob(self, blob_id: str, auth_token: str) -> None:
+        response = requests.get(
+            f'{self.root_api}/blob/{blob_id}/roles',
+            headers = {'AuthToken': auth_token},)
+
+        _assert_status_ok_(response.status_code)
+        if response.status_code == 200:
+            print(f'{response.status_code}, data: {response.json()}')
+        else:
+            print(f'{response.status_code}, {response.text}')
+
+    def post_patch_roles_blob(self, blob_id: str, auth_token:str ,new_roles: List[str], request: str) -> None:
+        if request == 'PATCH':
+            response = requests.patch(
+                f'{self.root_api}/blob/{blob_id}/roles',
+                headers = {'AuthToken': auth_token},
+                json = {
+                    'writable_by': new_roles
+                }
+            )
+            _assert_status_ok_(response.status_code)
+            print(f'Status Code Patch_Name Request: {response.status_code}')
+
+        if request == 'POST':
+            response = requests.post(
+                f'{self.root_api}/blob/{blob_id}/roles',
+                headers = {'AuthToken': auth_token},
+                json = {
+                    'writable_by': new_roles
+                }
+            )
+            _assert_status_ok_(response.status_code)
+            print(f'Status Code Post_Name Request: {response.status_code}')
+
+    def get_name_blob(self, blob_id: str, auth_token: str) -> None:
+        response = requests.get(
+            f'{self.root_api}/blob/{blob_id}/name',
+            headers = {'AuthToken': auth_token},)
+        _assert_status_ok_(response.status_code)
+        if response.status_code == 200:
+            print(f'{response.status_code}, name: {response.text}')
+        else:
+            print(f'{response.status_code}, {response.text}')
+
+    def post_patch_names_blob(self, blob_id: str, auth_token: str, name: str, request: str) -> None:
+        if request == 'PATCH':
+            response = requests.patch(
+                f'{self.root_api}/blob/{blob_id}/name',
+                headers = {'AuthToken': auth_token},
+                json = {
+                    'name': name
+                }
+            )
+            _assert_status_ok_(response.status_code)
+            print(f'{response.status_code}.{response.text}')
+
+        if request == 'POST':
+            response = requests.post(
+                f'{self.root_api}/blob/{blob_id}/name',
+                headers = {'AuthToken': auth_token},
+                json = {
+                    'name': name
+                }
+            )
+            _assert_status_ok_(response.status_code)
+            print(f'{response.status_code}, {response.text}')
+if __name__ == '__main__':
+    client = Client('http://192.168.56.110/api/v1')
+    writted_by = ['admin', 'Paco', 'Antonio']
+    blob_id = client.put_blob('token_for_admin', 'blob_2', writted_by, "hola.txt")
+    # print(f'Blob ID: {blob_id}')
+    # client.delete_blob(blob_id, 'token_for_admin')
+    #client.get_blob('04e985e7a5e0f5b5', 'token_for_admin')
+    #client.post_patch_blob('69093d05cb466d29', 'token_for_admin', '/home/sergio/Escritorio/prueba2.txt', 'POST')
+    #client.post_patch_blob('36ef169ddf93b54d', 'token_for_admin', '/home/sergio/Escritorio/prueba.txt', 'PATCH')
+    #client.get_roles_blob('69093d05cb466d29', 'token_for_admin')
+    #client.post_patch_roles_blob('69093d05cb466d29', 'token_for_admin' ,['jesus', 'fran', 'jaime'], 'POST')
+    #client.post_patch_roles_blob('06db2be4c3c10c78', ['paco', 'admin', 'antonio'], 'PATCH')
+    #client.get_name_blob('69093d05cb466d29', 'token_for_admin')
+    #client.post_patch_names_blob('69093d05cb466d29', 'token_for_admin', 'blob_3', 'POST')
--- trafficmix.py
+++ trafficmix.py
@@ -2,6 +2,8 @@
 import hashlib
 import unittest
 import requests
+import clientblob
+import os
 
 ADMIN_USERNAME = 'administrator'
 ADMIN_PASS_HASH = hashlib.sha256('admin'.encode()).hexdigest()
@@ -10,19 +12,28 @@ USER_USERNAME = 'user'
 USER_PASS_HASH = hashlib.sha256('userpass'.encode()).hexdigest()
 USER_PASS_CODE = hashlib.sha256(f'{USER_USERNAME}{USER_PASS_HASH}'.encode()).hexdigest()
 #URI_AUTH = 'http://miservicio:30080/auth/auth/v1'
+#URI_AUTH = 'http://10.0.2.8/auth/v1'
 URI_AUTH = 'http://192.168.56.110/auth/v1'
 #URI_TOKEN = 'http://miservicio:30080/token/api/v1'
+#URI_TOKEN = 'http://10.0.2.8/api/v1'
 URI_TOKEN = 'http://192.168.56.110/api/v1'
+URI_BLOB = 'http://192.168.56.110/api/v1'
 
 class TestTrafficMix(unittest.TestCase):
     def test_traffic_mix_auth(self):
-        
+        print("Test traffic mix")
+        print(f"Creando Token para el usuario {ADMIN_USERNAME}, con pass_hash {ADMIN_PASS_HASH}")
+        print(f"PUT {URI_TOKEN}/token , json={{'username': {ADMIN_USERNAME}, 'pass_hash': {ADMIN_PASS_HASH}}}")
         response = requests.put(URI_TOKEN + "/token", json={"username": ADMIN_USERNAME, "pass_hash": ADMIN_PASS_HASH}, timeout=5)
+        print(f"Respuesta: {response.status_code}, {response.json()} \n")
         self.assertEqual(response.status_code, 200)
-        
+
         token_admin =response.json()["token"]
         #comprobar que se ha creado el token
+        print(f"Pedimos informacion del token {token_admin}")
+        print(f"GET {URI_TOKEN}/token/{token_admin}")
         response = requests.get(URI_TOKEN+f"/token/{token_admin}",timeout=5)
+        print(f"Respuesta: {response.status_code}, {response.json()} \n")
         self.assertEqual(response.status_code, 200)
         #comprobar que el username de la respuesta
         self.assertEqual(response.json()["username"], ADMIN_USERNAME)
@@ -30,35 +41,109 @@ class TestTrafficMix(unittest.TestCase):
         self.assertIn("admin", response.json()["roles"])
 
         #eliminamos previamente el usuario nacho(sin comprobar si existe solo para asegurarnos que no existe)
+        print(f"Eliminamos prviamente el usuario nacho")
         response = requests.delete(URI_AUTH + f"/user/nacho",headers={"Content-Type": "application/json","AuthToken":token_admin}, timeout=10)
         # Creamos un nuevo usuario nacho
+        print(f"Creamos un nuevo usuario nacho")
+        print(f"PUT {URI_AUTH}/user , json={{'username': 'nacho', 'password': 'nachopass', 'role':'user'}}")
         response = requests.put(URI_AUTH + "/user",headers={"Content-Type": "application/json","AuthToken":token_admin}, json={"username": "nacho", "password": "nachopass", 'role':'user'} )
+        print(f"Respuesta: {response.status_code} \n")
         self.assertIn(response.status_code, [200, 201,204])
+
         # Obtenemos el usuario nacho
+        print(f"Obtenemos el usuario nacho")
+        print(f"GET {URI_AUTH}/user/nacho")
         response = requests.get(URI_AUTH + "/user/nacho",headers={"Content-Type": "application/json","AuthToken":token_admin}, timeout=5)
+        print(f"Respuesta: {response.status_code}, {response.json()} \n")
         self.assertEqual(response.status_code, 200)
         #comprobamos que el json de respuesta tiene el username nacho y dentro del array roles el rol user
         self.assertEqual(response.json()["username"], "nacho")
         self.assertIn("user", response.json()["role"])
 
         #Creamos un token para el usuario nacho
+        print(f"Creando Token para el usuario nacho, con password 'nachopass' y con pass_hash {hashlib.sha256('nachopass'.encode()).hexdigest()}")
         pass_hash = hashlib.sha256('nachopass'.encode()).hexdigest()
+        print(f"PUT {URI_TOKEN}/token , json={{'username': 'nacho', 'pass_hash': {pass_hash}}}")
         response = requests.put(URI_TOKEN + "/token", json={"username":    "nacho", "pass_hash": pass_hash}, timeout=5)
+        print(f"Respuesta: {response.status_code}, {response.json()} \n")
         self.assertEqual(response.status_code, 200)
         token_nacho =response.json()["token"]
 
         # Modificamos el usuario nacho con la password supernachopass y el rol de admin
+        print(f"Modificamos el usuario nacho con la password supernachopass y el rol de admin")
+        print(f"POST {URI_AUTH}/user/nacho , json={{'username': 'nacho', 'password': 'supernachopass', 'role':'admin'}}")
         response = requests.post(URI_AUTH + "/user/nacho",headers={"Content-Type": "application/json","AuthToken":token_nacho}, json={"username": "nacho", "password": "supernachopass", 'role':'admin'} )
+        print(f"Respuesta: {response.status_code} \n")
         self.assertIn(response.status_code, [200, 201,204])
         # Obtenemos el usuario nacho
+        print(f"Obtenemos de nuevo el usuario nacho")
+        print(f"GET {URI_AUTH}/user/nacho") 
         response = requests.get(URI_AUTH + "/user/nacho",headers={"Content-Type": "application/json","AuthToken":token_nacho}, timeout=5)
+        print(f"Respuesta: {response.status_code}, {response.json()} \n")
         self.assertEqual(response.status_code, 200)
         #comprobamos que el json de respuesta tiene el username nacho y dentro del array roles el rol admin
         self.assertEqual(response.json()["username"], "nacho")
         self.assertIn("admin", response.json()["role"])
 
+        # Verificamos el usuario nacho con la password supernachopass
+        print(f"Verificamos el usuario nacho con la password supernachopass")
+        nacho_pass_hash = hashlib.sha256('supernachopass'.encode()).hexdigest()
+        nacho_pass_code = hashlib.sha256(f'nacho{nacho_pass_hash}'.encode()).hexdigest()
+        print(f"GET {URI_AUTH}/is_authorized/{nacho_pass_code}")
+        response = requests.get(URI_AUTH + f"/is_authorized/{nacho_pass_code}", timeout=5)
+        print(f"Respuesta: {response.status_code} \n")
+        self.assertEqual(response.status_code, 200)
+        #comprobamos que el rol de admin esta en roles
+        self.assertIn("admin", response.json()["roles"])
+
+        
+
+
+        # Probamos el servicio de blobs
+
+        #Creamos un archivo hola.txt
+        print("Creamos un archivo hola.txt, con el contenido 'Hola Mundo, esto es un blob'")
+        with open("hola.txt", "w") as f:
+            f.write("Hola Mundo, esto es un blob")
+        #Creamos un blob con el archivo hola.txt
+        print("Creamos un blob con el archivo hola.txt")
+
+        client = clientblob.Client(URI_BLOB)
+        writted_by = ['admin', 'Paco', 'Antonio']
+        print(f"PUT {URI_TOKEN}/blob , json={{'name': 'blob_admin', 'writable_by': {writted_by}, 'filename': 'hola.txt'}}")
+        blob_id = client.put_blob(token_admin, 'blob_admin', writted_by, "hola.txt")
+        #Borramos el archivo hola.txt
+        print("Borramos el archivo original hola.txt")
+        os.remove("hola.txt")
+        #obtenemos el blob
+        print(f"Obtenemos el blob {blob_id}")
+        print(f"GET {URI_TOKEN}/blob/{blob_id}")
+        client.get_blob(blob_id, token_admin)
+        print()
+        #obtenemos los roles del blob
+        print(f"Obtenemos los roles del blob {blob_id}")
+        print(f"GET {URI_TOKEN}/blob/{blob_id}/roles")
+        client.get_roles_blob(blob_id, token_admin)
+        print()
+
+        #obtenemos el nombre del blob
+        print(f"Obtenemos el nombre del blob {blob_id}")
+        print(f"GET {URI_TOKEN}/blob/{blob_id}/name")
+        client.get_name_blob(blob_id, token_admin)
+        print()
+
+        #borramos el blob
+        print(f"Borramos el blob {blob_id}")
+        print(f"DELETE {URI_TOKEN}/blob/{blob_id}")
+        client.delete_blob(blob_id, token_admin)
+        print()
+
+
         # Borramos el usuario nacho
+        print(f"Borramos el usuario nacho")
+        print(f"DELETE {URI_AUTH}/user/nacho")
         response = requests.delete(URI_AUTH + "/user/nacho",headers={"Content-Type": "application/json","AuthToken":token_nacho}, timeout=5)
+        print(f"Respuesta: {response.status_code} \n")
         self.assertEqual(response.status_code, 204)
 
 
